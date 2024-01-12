29e1da15e21114b63678490d712e584b3f7de418
ridwanmithu <ridwan.mithu@outlook.com>
2024-01-12 22:43:27 +0600

    navbar and hero

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import HeroSection from './Component/HeroSection'
+import Navbar from './Component/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />,
+      <HeroSection/>
+
+    </>
+  )
+}
+
+export default App
